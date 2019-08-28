aee2177b1bd987dd54c55879189b08a9b275f843
Ben Shealy <bentshermann@gmail.com>
2019-08-28 14:53:42 -0400

    Changed trace and nvprof options to be optional in aggregate.py

--- bin/aggregate.py
+++ bin/aggregate.py
@@ -46,60 +46,62 @@ if __name__ == "__main__":
 
 	args = parser.parse_args()
 
-	# load nextflow trace files
-	trace_files = [load_trace(filename) for filename in args.trace_input]
+	if args.trace_input:
+		# load nextflow trace files
+		trace_files = [load_trace(filename) for filename in args.trace_input]
 
-	# aggregate trace files into one dataframe
-	X_trace = pd.DataFrame()
+		# aggregate trace files into one dataframe
+		X_trace = pd.DataFrame()
 
-	for X_i in trace_files:
-		X_trace = X_trace.append(X_i, sort=False)
+		for X_i in trace_files:
+			X_trace = X_trace.append(X_i, sort=False)
 
-	# append condition columns to trace dataframe
-	X_trace["tag"] = X_trace["tag"].apply(lambda tag: tag.replace(".", "-"))
+		# append condition columns to trace dataframe
+		X_trace["tag"] = X_trace["tag"].apply(lambda tag: tag.replace(".", "-"))
 
-	X_trace["experiment_type"] = X_trace["process"]
-	X_trace["experiment_value"] = X_trace["tag"].apply(lambda tag: tag.split("/")[0])
-	X_trace["dataset"] = X_trace["tag"].apply(lambda tag: tag.split("/")[1])
-	X_trace["gpu_model"] = X_trace["tag"].apply(lambda tag: tag.split("/")[2])
-	X_trace["trial"] = X_trace["tag"].apply(lambda tag: tag.split("/")[3])
+		X_trace["experiment_type"] = X_trace["process"]
+		X_trace["experiment_value"] = X_trace["tag"].apply(lambda tag: tag.split("/")[0])
+		X_trace["dataset"] = X_trace["tag"].apply(lambda tag: tag.split("/")[1])
+		X_trace["gpu_model"] = X_trace["tag"].apply(lambda tag: tag.split("/")[2])
+		X_trace["trial"] = X_trace["tag"].apply(lambda tag: tag.split("/")[3])
 
-	X_trace.drop(columns=["process", "tag", "name"], inplace=True)
+		X_trace.drop(columns=["process", "tag", "name"], inplace=True)
 
-	# save trace dataframe
-	X_trace.to_csv(args.trace_output, sep="\t", index=False)
+		# save trace dataframe
+		X_trace.to_csv(args.trace_output, sep="\t", index=False)
 
-	# load nvprof files
-	nvprof_files = [load_nvprof(filename) for filename in args.nvprof_input]
+	if args.nvprof_input:
+		# load nvprof files
+		nvprof_files = [load_nvprof(filename) for filename in args.nvprof_input]
 
-	# aggregate nvprof files into one dataframe
-	X_nvprof = pd.DataFrame()
+		# aggregate nvprof files into one dataframe
+		X_nvprof = pd.DataFrame()
 
-	for filename, X_i in zip(args.nvprof_input, nvprof_files):
-		# parse conditions from filename
-		tokens = filename.replace(".", "/").split("/")
-		experiment_type = tokens[-7]
-		experiment_value = tokens[-6]
-		dataset = tokens[-5]
-		gpu_model = tokens[-4]
-		trial = tokens[-3]
+		for filename, X_i in zip(args.nvprof_input, nvprof_files):
+			# parse conditions from filename
+			tokens = filename.replace(".", "/").split("/")
+			experiment_type = tokens[-7]
+			experiment_value = tokens[-6]
+			dataset = tokens[-5]
+			gpu_model = tokens[-4]
+			trial = tokens[-3]
 
-		# append condition columns to input dataframe
-		X_i["experiment_type"] = experiment_type
-		X_i["experiment_value"] = experiment_value
-		X_i["dataset"] = dataset
-		X_i["gpu_model"] = gpu_model
-		X_i["trial"] = trial
+			# append condition columns to input dataframe
+			X_i["experiment_type"] = experiment_type
+			X_i["experiment_value"] = experiment_value
+			X_i["dataset"] = dataset
+			X_i["gpu_model"] = gpu_model
+			X_i["trial"] = trial
 
-		# append input dataframe rows to output dataframe
-		X_nvprof = X_nvprof.append(X_i, sort=False)
+			# append input dataframe rows to output dataframe
+			X_nvprof = X_nvprof.append(X_i, sort=False)
 
-	# rename column names if mapping file is specified
-	if args.nvprof_mapper:
-		mapper = pd.read_csv(args.nvprof_mapper, sep="\t")
-		mapper = {mapper.loc[i, "display_name"]: mapper.loc[i, "column_name"] for i in mapper.index}
+		# rename column names if mapping file is specified
+		if args.nvprof_mapper:
+			mapper = pd.read_csv(args.nvprof_mapper, sep="\t")
+			mapper = {mapper.loc[i, "display_name"]: mapper.loc[i, "column_name"] for i in mapper.index}
 
-		X_nvprof.rename(columns=mapper, copy=False, inplace=True)
+			X_nvprof.rename(columns=mapper, copy=False, inplace=True)
 
-	# save nvprof dataframe
-	X_nvprof.to_csv(args.nvprof_output, sep="\t", index=False)
+		# save nvprof dataframe
+		X_nvprof.to_csv(args.nvprof_output, sep="\t", index=False)
