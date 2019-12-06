f6294227268f307d2e216e837dc093a231bccb75
Ben Shealy <bentshermann@gmail.com>
2019-12-06 11:29:49 -0500

    Added histograms and countplots to visualize.py

--- bin/visualize.py
+++ bin/visualize.py
@@ -19,15 +19,16 @@ if __name__ == "__main__":
 	parser.add_argument("input", help="input dataset")
 	parser.add_argument("outfile", help="output plot")
 	parser.add_argument("--xaxis", help="column name of x-axis", required=True)
-	parser.add_argument("--yaxis", help="column name of y-axis", required=True)
-	parser.add_argument("--hue1", help="column name of primary hue axis (splits data within subplot)", nargs="?")
-	parser.add_argument("--hue2", help="column name of secondary hue axis (splits data across subplots)", nargs="?")
+	parser.add_argument("--yaxis", help="column name of y-axis", nargs="?")
+	parser.add_argument("--row", help="column name of row-wise category", nargs="?")
+	parser.add_argument("--col", help="column name of column-wise category", nargs="?")
+	parser.add_argument("--hue", help="column name of hue category", nargs="?")
 	parser.add_argument("--select", help="select a set of values from a column", action="append", default=[], metavar="column=value,value,...")
 	parser.add_argument("--mapper", help="mappping file of display names for axis columns", nargs="?")
 	parser.add_argument("--mapper-term", help="additional display name mapping (overwrites mapping file)", action="append", default=[], metavar="column_name=display_name")
 	parser.add_argument("--color", help="color for all barplot elements", nargs="?")
 	parser.add_argument("--palette", help="palette for all barplot elements", nargs="?")
-	parser.add_argument("--ratio", help="aspect ratio to control figure width", type=float, default=0)
+	parser.add_argument("--aspect", help="aspect ratio to control figure width", type=float, default=0)
 	parser.add_argument("--sharey", help="whether to use uniform y-axis across subplots", action="store_true")
 
 	args = parser.parse_args()
@@ -39,8 +40,9 @@ if __name__ == "__main__":
 	axes = [
 		args.xaxis,
 		args.yaxis,
-		args.hue1,
-		args.hue2
+		args.row,
+		args.col,
+		args.hue
 	]
 
 	for column in axes:
@@ -78,39 +80,53 @@ if __name__ == "__main__":
 
 	args.xaxis = mapper[args.xaxis] if args.xaxis in mapper else args.xaxis
 	args.yaxis = mapper[args.yaxis] if args.yaxis in mapper else args.yaxis
-	args.hue1 = mapper[args.hue1] if args.hue1 in mapper else args.hue1
-	args.hue2 = mapper[args.hue2] if args.hue2 in mapper else args.hue2
+	args.row = mapper[args.row] if args.row in mapper else args.row
+	args.col = mapper[args.col] if args.col in mapper else args.col
+	args.hue = mapper[args.hue] if args.hue in mapper else args.hue
 	data.rename(columns=mapper, copy=False, inplace=True)
 
 	# apply aspect ratio if specified
-	if args.ratio != 0:
-		plt.figure(figsize=(5 * args.ratio, 5))
+	if args.aspect != 0:
+		plt.figure(figsize=(5 * args.aspect, 5))
 
-	# create a categorical plot if either hue columns are specified
-	if args.hue1 != None or args.hue2 != None:
-		sns.catplot(
-			x=args.xaxis,
-			y=args.yaxis,
-			hue=args.hue1,
-			col=args.hue2,
+	# create a facet grid for plotting
+	g = sns.FacetGrid(
+		data,
+		row=args.row,
+		col=args.col,
+		sharey=args.sharey)
+
+	# use bar plots if y-axis is specified
+	if args.yaxis != None:
+		g = g.map(
+			sns.barplot,
+			args.xaxis,
+			args.yaxis,
+			hue=args.hue,
 			data=data,
-			kind="bar",
 			ci=68,
 			color=args.color,
-			palette=args.palette,
-			sharey=args.sharey
-		)
+			palette=args.palette)
+		g.add_legend()
 
-	# otherwise create a bar plot
+	# otherwise use distribution plots
 	else:
-		sns.barplot(
-			x=args.xaxis,
-			y=args.yaxis,
-			data=data,
-			ci=68,
-			color=args.color,
-			palette=args.palette
-		)
+		# use histograms for continuous data
+		if data[args.xaxis].dtype.kind in "fcmM":
+			g.map(
+				sns.distplot,
+				args.xaxis,
+				color=args.color,
+				norm_hist=False)
+
+		# use count plots for categorical data
+		else: # "biuOSUV"
+			g.map(
+				sns.countplot,
+				args.xaxis,
+				hue=args.hue,
+				color=args.color,
+				palette=args.palette)
 
 	# disable x-axis ticks if there are too many categories
 	if len(set(data[args.xaxis])) >= 100:
