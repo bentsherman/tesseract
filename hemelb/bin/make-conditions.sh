fe2461b1f785fa9c94f0d83c0799cb733aae2e13
Ben Shealy <bentshermann@gmail.com>
2020-04-02 14:14:00 -0400

    Updated hemelb make-conditions.py

--- hemelb/bin/make-conditions.sh
+++ hemelb/bin/make-conditions.sh
@@ -2,11 +2,11 @@
 # Generate conditions file for some basic hemelb experiments.
 
 python bin/make-conditions.py \
-	--default blocksize=16 \
-	--default gpu_model=1080ti \
+	--default blocksize=32 \
+	--default gpu_model=p100 \
 	--default latticetype=D3Q15 \
 	--default np=1 \
 	--experiment blocksize=16,32,64,128,256,512 \
 	--experiment latticetype=D3Q15,D3Q19,D3Q27 \
-	--experiment gpu_model=cpu,1080ti np=1,2,4,8 \
+	--experiment gpu_model=cpu,p100,v100 np=1,2,4,8,16,32 \
 	--remove-duplicates
